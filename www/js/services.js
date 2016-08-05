83da4bbe0dd30fc97299f2888e195fdc4bb6721b
hiepnm <hiepnm2909@gmail.com>
2016-08-05 10:03:50 +0700

    Fix Can't load aberto.png

--- www/js/services.js
+++ www/js/services.js
@@ -1,7 +1,8 @@
 'use strict';
 
 angular.module('conFusion.services', ['ngResource'])
-.constant("baseURL","http://localhost:3000/")
+//.constant("baseURL","http://localhost:3000/")
+.constant("baseURL","http://192.168.1.14:3000/")
 .service('menuFactory', ['$resource', 'baseURL', function($resource,baseURL) {
   this.getDishes = function(){
     return $resource(baseURL+"dishes/:id",null,  {'update':{method:'PUT' }});
